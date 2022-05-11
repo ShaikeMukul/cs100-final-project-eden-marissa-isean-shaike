f59630233ac9044128628153dd3c3fcac3795127
Isean Bhanot <Isean4B@gmail.com>
2022-05-11 12:26:33 -0700

    Coded Inventory.h file

---
+++ Headers/Inventory.h
@@ -0,0 +1,18 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "Items.h"
+#include <vector>
+
+class Inventory{
+    private:
+        vector<Items> itemList;
+        int numItems;
+    protected:
+        void addItem(Items item);
+        void removeItem(Items item);
+    public:
+        std::string printItems();
+};
+
+#endif
--- Headers/Items.h
+++ Headers/Items.h
@@ -1,18 +1,22 @@
 #ifndef ITEMS_H
 #define ITEMS_H
 
+#include <string>
+
 class Items{
-    protected:
+    private:
         double healthEffect;
         double damageEffect;
+        String tag;
         std::string itemName;
         std::string itemDescription;
     public:
         Item(std::string name, std::string description, double healthAffect, double damageAffect) : healthEffect(healthAffect), healthEffect(damageAffect), itemName(name), itemDescription(description) {}
         std::string getItemDescription();
         std::string getItemName();
+        std::string getItemTag();
         double getHealthEffect();
         double getDamageEffect();
-}
+};
 
 #endif 
