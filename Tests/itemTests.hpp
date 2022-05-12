5f91a25ef6e0792fb69b159870a47c6c26f10e1a
Isean Bhanot <Isean4B@gmail.com>
2022-05-11 20:26:40 -0700

    Finished Item class tests

--- Tests/itemTests.hpp
+++ Tests/itemTests.hpp
@@ -10,10 +10,26 @@ TEST(ItemSuite, Constructor){
     EXPECT_EQ(apple.getItemName(),"name");
 }
 
-TEST(ItemSuite, ParmConstructor){
+TEST(ItemSuite, ParmConstructorHealth){
     Items healthPotion("Health Potion", "Heals the player by a certain amount", 25.32, 0);
     EXPECT_EQ(healthPotion.getDamageEffect(), 0);
     EXPECT_EQ(healthPotion.getHealthEffect(), 25.32);
     EXPECT_EQ(healthPotion.getItemDescription(), "Heals the player by a certain amount");
     EXPECT_EQ(healthPotion.getItemName(),"Health Potion");
+}
+
+TEST(ItemSuite, ParmConstructorNegHealth){
+    Items damagePotion("Damage Potion", "Damages the player by a certain amount", -25, 0);
+    EXPECT_EQ(damagePotion.getDamageEffect(), 0);
+    EXPECT_EQ(damagePotion.getHealthEffect(), -25);
+    EXPECT_EQ(damagePotion.getItemDescription(), "Damages the player by a certain amount");
+    EXPECT_EQ(damagePotion.getItemName(),"Damage Potion");
+}
+
+TEST(ItemSuite, ParmConstructorHealthAndDamage){
+    Items weirdPotion("Weird Potion", "Weird effects on Player", -123.32, -2.231);
+    EXPECT_EQ(weirdPotion.getDamageEffect(), -2.231);
+    EXPECT_EQ(weirdPotion.getHealthEffect(), -123.32);
+    EXPECT_EQ(weirdPotion.getItemDescription(), "Weird effects on Player");
+    EXPECT_EQ(weirdPotion.getItemName(),"Weird Potion");
 }
