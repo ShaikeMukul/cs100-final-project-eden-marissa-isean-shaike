4caec7b1a4c8b26d6133846af25db3ef13b07dba
Isean Bhanot <Isean4B@gmail.com>
2022-05-11 23:34:17 -0700

    added final tests for GetItem function

--- Tests/inventoryTests.hpp
+++ Tests/inventoryTests.hpp
@@ -21,12 +21,29 @@ TEST(InventorySuite, addMultipleItems){
     EXPECT_EQ(test1.getSize(), 10);
 }
 
-TEST(InventorySuite, verifyGet){
+TEST(InventorySuite, verifyGetOneSizedInventory){
     Inventory test1;
     test1.addItem(Items("Health Potion", "Heals the player by a certain amount", 25.32, 0));
-    EXPECT_EQ(test1.getItem(0).getDamageEffect(), 0);
-    EXPECT_EQ(test1.getItem(0).getHealthEffect(), 25.32);
-    EXPECT_EQ(test1.getItem(0).getItemDescription(), "Heals the player by a certain amount");
-    EXPECT_EQ(test1.getItem(0).getItemName(),"Health Potion");
+    EXPECT_EQ(test1.getItem("Health Potion").getDamageEffect(), 0);
+    EXPECT_EQ(test1.getItem("Health Potion").getHealthEffect(), 25.32);
+    EXPECT_EQ(test1.getItem("Health Potion").getItemDescription(), "Heals the player by a certain amount");
+    EXPECT_EQ(test1.getItem("Health Potion").getItemName(),"Health Potion");
+}
+
+TEST(InventorySuite, verifyGetLargeSizedInventory){
+    Inventory test1;
+    test1.addItem(Items("Health Potion", "Heals", 25.32, 0));
+    test1.addItem(Items("Damage Potion", "Damage", -123, 213));
+    test1.addItem(Items("Potion Potion", "Potion", 0, 0));
+    test1.addItem(Items("Pop Potion", "Potion", 123.12, 0));
+    test1.addItem(Items("Lol Potion", "asdsad", 546.45, 123));
+    test1.addItem(Items("No Potion", "11323", 12, -123.123));
+    test1.addItem(Items("Zero Potion", "vdsa", 654, .4565));
+
+    EXPECT_EQ(test1.getItem("Health Potion").getDamageEffect(), 0);
+    EXPECT_EQ(test1.getItem("Zero Potion").getHealthEffect(), 654);
+    EXPECT_EQ(test1.getItem("Pop Potion").getItemDescription(), "Potion");
+    EXPECT_EQ(test1.getItem("Potion Potion").getItemName(),"Potion Potion");
+
 }
 
