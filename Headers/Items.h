2b0bbf10991db700579da45ec030095aa1480a41
Isean Bhanot <Isean4B@gmail.com>
2022-05-11 20:22:22 -0700

    Item class test added
    ParmConstructor Test with a health potion

--- Headers/Items.h
+++ Headers/Items.h
@@ -7,15 +7,13 @@ class Items{
     private:
         double healthEffect;
         double damageEffect;
-        std::string tag;
         std::string itemName;
         std::string itemDescription;
     public:
-        Items(std::string tagInput,std::string name, std::string description, double healthAffect, double damageAffect) : healthEffect(healthAffect), damageEffect(damageAffect), itemName(name), itemDescription(description), tag(tagInput) {}
-        Items() : tag("tag"), healthEffect(0), damageEffect(0), itemName("name"), itemDescription("description") {}
+        Items(std::string name, std::string description, double healthAffect, double damageAffect) : healthEffect(healthAffect), damageEffect(damageAffect), itemName(name), itemDescription(description){}
+        Items() : healthEffect(0), damageEffect(0), itemName("name"), itemDescription("description") {}
         std::string getItemDescription(){ return (itemDescription);}
         std::string getItemName(){ return (itemName);}
-        std::string getItemTag(){ return (tag);}
         double getHealthEffect(){ return (healthEffect);}
         double getDamageEffect(){ return (damageEffect);}
 };
--- Tests/itemTests.hpp
+++ Tests/itemTests.hpp
@@ -8,5 +8,12 @@ TEST(ItemSuite, Constructor){
     EXPECT_EQ(apple.getHealthEffect(), 0);
     EXPECT_EQ(apple.getItemDescription(), "description");
     EXPECT_EQ(apple.getItemName(),"name");
-    EXPECT_EQ(apple.getItemTag(), "tag");
+}
+
+TEST(ItemSuite, ParmConstructor){
+    Items healthPotion("Health Potion", "Heals the player by a certain amount", 25.32, 0);
+    EXPECT_EQ(healthPotion.getDamageEffect(), 0);
+    EXPECT_EQ(healthPotion.getHealthEffect(), 25.32);
+    EXPECT_EQ(healthPotion.getItemDescription(), "Heals the player by a certain amount");
+    EXPECT_EQ(healthPotion.getItemName(),"Health Potion");
 }
