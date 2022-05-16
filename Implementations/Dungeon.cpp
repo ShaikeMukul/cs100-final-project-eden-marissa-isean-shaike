fc150585cc75b4c4d9381ca7e13895a26ef136c1
ShaikeMukul <smuku004@ucr.edu>
2022-05-15 21:46:22 -0700

    dungeon file initialize

---
+++ Implementations/Dungeon.cpp
@@ -0,0 +1,32 @@
+#include "../Headers/Dungeon.h"
+
+Dungeon::Dungeon(){
+
+}
+Dungeon::~Dungeon(){
+
+}
+std::string Dungeon::getCurrEntities() {
+
+}
+
+Entities Dungeon::attack(){
+
+}
+
+Entities Dungeon::getDeath() {
+
+}
+Entities Dungeon::getDeath(){
+
+}
+void Dungeon::loadEnemy(){
+
+}
+int Dungeon::getCurrLevel() {
+
+}
+void Dungeon::nextFloor() {
+
+}
+
