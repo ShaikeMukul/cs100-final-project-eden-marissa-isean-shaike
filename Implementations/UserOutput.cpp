5c79bde3fb2cd4a6d3695e4eb57e426e5037c510
ShaikeMukul <smuku004@ucr.edu>
2022-05-17 09:34:24 -0700

    print scene added

--- Implementations/UserOutput.cpp
+++ Implementations/UserOutput.cpp
@@ -14,5 +14,11 @@ std::string UserOutput::printAdvancement() {
 
 }
 std::string UserOutput::printScene(){
-
-}
+    std::cout << "Printing Scene: " << std::endl;
+    std::cout << "Player Name : " << name << std::endl;
+    std::cout << "Current Level : " << level << std::endl;
+    std::cout << "Strength : " << strength << std::endl;
+    std::cout << "Enemy : " << enemy << std::endl;
+    std::cout << "Damage : " << damage << std::endl;
+    std::cout << std::endl;
+};
