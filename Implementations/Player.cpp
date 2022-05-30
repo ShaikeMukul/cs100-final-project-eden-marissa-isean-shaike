f31cafe4d606976f5f50df805a7c235527314b47
ShaikeMukul <smuku004@ucr.edu>
2022-05-29 23:10:58 -0700

    player class init

---
+++ Implementations/Player.cpp
@@ -0,0 +1,23 @@
+#include "../Headers/Player.h"
+#include <iostream>
+
+Player::Player() {
+    
+}
+
+std::string Player::setName(std::string name){
+}
+
+std::string Player::getName() {
+}
+
+void Player::changeHealth(){
+}
+
+void Player::changeDamage(){
+}
+
+bool Player::levelUp(){
+
+}
+
