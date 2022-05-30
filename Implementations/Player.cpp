969c45c82f3843c9a9d448fc6972485460507d6b
ShaikeMukul <smuku004@ucr.edu>
2022-05-29 23:12:47 -0700

    setName function added

--- Implementations/Player.cpp
+++ Implementations/Player.cpp
@@ -2,10 +2,16 @@
 #include <iostream>
 
 Player::Player() {
+    this->name= "player";
+    this->level = 0;
+    this->damage = 0;
+    this->health=0;
+    this->description="This player is in the dark dungeon"
     
 }
 
 std::string Player::setName(std::string name){
+    name = name;
 }
 
 std::string Player::getName() {
